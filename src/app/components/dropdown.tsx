2938c02221471b1971b2e16ed9fda81dc3f6100d
Agsha Athalla Nurkareem <agshaathalla@gmail.com>
2024-03-05 20:35:25 +0700

    feat(Front Office): Tambah Antrean

---
+++ src/app/components/dropdown.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import { IoMdArrowDropdown } from "react-icons/io";
+
+const Dropdown = ({ options, onSelect }:any) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionClick = (option:any) => {
+    setSelectedOption(option);
+    onSelect(option);
+    setIsOpen(false);
+  };
+
+  const dropdownClasses = classNames(
+    'origin-top-right right-0 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5',
+    {
+      'max-h-60 overflow-y-auto': options.length > 10,
+    }
+  );
+
+  return (
+    <div className="relative inline-block text-left w-96">
+      <button
+        type="button"
+        className="inline-flex justify-between items-center w-full p-2 border rounded-2xl shadow-sm text-sm  bg-white text-shade8 font-Poppins font-semibold hover:text-tint7 hover:bg-shade4 h-12"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {selectedOption ? selectedOption.label : 'Select an option'}
+        <IoMdArrowDropdown />
+      </button>
+
+      {isOpen && (
+        <div className={dropdownClasses} style={{ zIndex: 1000 }}>
+          <div className="py-1 w-full">
+            {options.map((option:any) => (
+              <button
+                key={option.value}
+                className={classNames(
+                  'flex flex-col w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900',
+                  { 'bg-gray-100': option === selectedOption }
+                )}
+                onClick={() => handleOptionClick(option)}
+              >
+                {option.label}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
---
+++ src/app/tambah-antrian/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Image from 'next/image';
+import Background from '../../../public/pattern.svg';
+import Dropdown from '../components/dropdown';
+import { useState } from 'react';
+export default function TambahAntrian() {
+    const options = [
+        { label: 'Poli Umum', value: 'umum' },
+        { label: 'Poli Gigi', value: 'gigi' },
+        { label: 'Bidan', value: 'bidan' },
+    ]
+    const [selectedOption, setSelectedOption] = useState(null);
+    const handleOptionClick = (option:any) => {
+        setSelectedOption(option);
+    };
+
+
+    return (
+        <div className='flex flex-row mr-10 bg-primary1'>
+            <Image 
+                src={Background} 
+                alt="Background" 
+                className=" h-screen"
+            />
+            <div className=' text-shade6 font-poppins w-auto'>
+                <p className=' text-5xl font-extrabold mt-20 mb-4'>DAFTAR BEROBAT</p>
+                <p className=' text-2xl font-medium'>Jika baru pertama kali mendaftar, silahkan ke menu pendaftaran pasien</p>
+                <div className=' mt-14'>
+                    <div>
+                        <p className=' text-2xl '>Nama</p>
+                        <input className="w-96 h-12 px-7 py-3.5 left-0 top-9 bg-gray-100 rounded-2xl border border-neutral-200 justify-start items-center gap-2.5 inline-flex text-shade7" placeholder="Masukkan nama pasien"/>
+                    </div>
+                    <div>
+                        <p className=' text-2xl mt-4 '>NIK</p>
+                        <input className="w-96 h-12 px-7 py-3.5 left-0 top-9 bg-gray-100 rounded-2xl border border-neutral-200 justify-start items-center gap-2.5 inline-flex text-shade7" placeholder="Masukkan NIK pasien"/>
+                    </div>
+                    <div>
+                        <p className=' text-2xl mt-4'>Poli</p>
+                        <Dropdown
+                        options={options}
+                        onSelect={handleOptionClick}
+                        />
+                    </div>
+                    <button className=' bg-primary1 h-11 w-36 rounded-3xl text-white font-poppins mt-4'>
+                        Lanjut
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
