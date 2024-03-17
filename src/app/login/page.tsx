2ce8933f90381eb9c6e7f132a26524af55e4b4ff
syauqijan <13521014@std.stei.itb.ac.id>
2024-03-17 15:20:43 +0700

    save cookie

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,16 +3,18 @@ import React, {useState} from 'react'
 import Image from "next/image";
 import Pattern from '../../../public/images/pattern.svg';
 import axios from "axios";
+import Cookies from "js-cookie";
+// import jwt_decode from 'jsonwebtoken';
+import { jwtDecode } from 'jwt-decode';
+
 
 
 const loginPage = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const [formData, setFormData] = useState({
         email: "",
         password: "",
-      });
+    });
     const validateEmail = (email:any) => {
         var re = /\S+@\S+\.\S+/;
         return re.test(email);
@@ -56,13 +58,31 @@ const validateForm = () => {
                 console.log(response);
                 if (response.status === 200) {
                   console.log("Success!");
-                  location.href = "/";
+                  Cookies.set('token', response.data.token, { expires: 1 }); 
+                    location.href = '/';
                 }
-              }
-              catch (error) {
-                console.log(error);
-              }
-        }
+                const token = Cookies.get('token');
+
+                if (token) {
+                    try {
+                        const decoded = jwtDecode(token);
+                        console.log('Payload Data:', decoded);
+                        
+                    } catch (error) {
+                        console.error('Gagal mendekode token:', error);
+                    }
+                } else {
+                    console.log('Cookie tidak ditemukan');
+                }
+                        
+                    }
+                    catch (error) {
+                        console.log(error);
+                    }
+                }
+
+
+        
 };
 
 return (
