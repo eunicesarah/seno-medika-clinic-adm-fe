012949ec7e04fdebe87488dd66f2d898e959e8d4
Agsha Athalla Nurkareem <agshaathalla@gmail.com>
2024-03-24 16:40:30 +0700

    feat(TestingFE): add login page testing

--- __test__/login.test.tsx
+++ __test__/login.test.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import LoginPage from '@/app/login/page'
+import axios from 'axios'
 
 describe('Login page', () => {
     it('should render login page', () => {
@@ -35,4 +36,62 @@ describe('Login page', () => {
         }))
 
     })
+
+    it('should display error message for blank email and password', () => {
+        render(<LoginPage />)
+        const submitButton = screen.getByText('Login')
+
+        fireEvent.click(submitButton)
+        expect(screen.getByText('Please enter your email')).toBeInTheDocument()
+        expect(screen.getByText('Please enter your password')).toBeInTheDocument()
+    })
+    
+    it('should display error message for invalid email', () => {
+        render(<LoginPage />)
+        const emailInput = screen.getByLabelText('E-mail')
+        const passwordInput = screen.getByLabelText('Kata Sandi')
+        const submitButton = screen.getByText('Login')
+
+        fireEvent.change(emailInput, { target: { value: 'invalid@email' } })
+        fireEvent.change(passwordInput, { target: { value: 'password' } })
+        fireEvent.click(submitButton)
+
+        expect(screen.getByText('Email is not valid')).toBeInTheDocument()
+    })
+    
+    it('should call axios when submit is clicked', async () => {
+
+        const mockedAxios = axios as jest.Mocked<typeof axios>; // Create a mocked version of axios
+
+        const fakeResponse = {
+            status: 200,
+            data: {
+            token: 'fakeToken123',
+            },
+        };
+
+        render(<LoginPage />)
+        const emailInput = screen.getByLabelText('E-mail')
+        const passwordInput = screen.getByLabelText('Kata Sandi')
+        const submitButton = screen.getByText('Login')
+
+        fireEvent.change(emailInput, { target: { value: 'valid@mail.com' } })
+        fireEvent.change(passwordInput, { target: { value: 'password' } })
+        fireEvent.click(submitButton)
+
+        await waitFor(() => {
+            expect(mockedAxios.post).toHaveBeenCalledTimes(1);
+            
+            expect(mockedAxios.post).toHaveBeenCalledWith(
+              'http://localhost:8080/login',
+              {
+                email: 'valid@mail.com',
+                password: 'password',
+              }
+            );
+
+    })
+
+    })
+
 })
