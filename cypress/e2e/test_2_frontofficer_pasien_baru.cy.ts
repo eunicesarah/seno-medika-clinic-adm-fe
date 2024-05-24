d93ff4d7a3f7845704f7ad299cc1e915d6303c54
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-05-24 15:03:00 +0700

    feat(testing): front office new patient

---
+++ cypress/e2e/test_2_frontofficer_pasien_baru.cy.ts
@@ -0,0 +1,51 @@
+describe('Front Officer', () => {
+    beforeEach(() => {
+        cy.viewport(1920, 1080);
+        cy.visit('http://localhost:3000/login');
+    });
+
+    it('displays the header correctly', () => {
+        cy.get('[data-testid=title]').should('contain', 'MASUK');
+    });
+
+    it('register new patient', () => {
+        cy.get('[data-testid="input-email"]').type('foahmada@gmail.com');
+        cy.get('[data-testid="input-password"]').type('123123qw');
+        cy.get('[data-testid="button-login"]').click();
+        cy.get('[data-testid="button-tambah-pasien"]').click();
+        cy.get('[data-testid="pasien-baru"]').click();
+        cy.get('[data-testid="input-name"]').type('Cantika');
+        cy.get('[data-testid="input-nik"]').type('330299110022992');
+        cy.get('[data-testid="input-no-kk"]').type('3399112200');
+        cy.get('[data-testid=dropdown-goldar]').click();
+        cy.get('[data-testid=dropdown-goldar]').contains('AB').click();
+        cy.get('[data-testid=gender]').contains('Laki-laki').click();
+        cy.get('[data-testid=input-tempat-lahir]').type('Jakarta');
+        cy.get('[data-testid=input-tanggal-lahir]').type('11/11/2000');
+        cy.get('[data-testid=input-email]').type('ahmada@gmail.com');
+        cy.get('[data-testid=input-phone]').type('08123456789');
+        cy.get('[data-testid=input-provinsi]').click();
+        cy.get('[data-testid=input-provinsi]').contains('ACEH').click();
+        cy.get('[data-testid=input-kabupaten-kota]').click();
+        cy.get('[data-testid=input-kabupaten-kota]').contains('KABUPATEN ACEH BARAT').click();
+        cy.get('[data-testid=input-kecamatan]').click();
+        cy.get('[data-testid=input-kecamatan]').contains('BUBON').click();
+        cy.get('[data-testid=input-kelurahan]').click();
+        cy.get('[data-testid=input-kelurahan]').contains('RAMBONG').click();
+        cy.get('[data-testid=input-alamat]').type('Jalan Rambong');
+        cy.get('[data-testid=input-warga-negara]').click();
+        cy.get('[data-testid=input-warga-negara]').contains('WNI').click();
+        cy.get('[data-testid=input-status-perkawinan]').click();
+        cy.get('[data-testid=input-status-perkawinan]').contains('Belum Kawin').click();
+        cy.get('[data-testid=input-pendidikan]').click();
+        cy.get('[data-testid=input-pendidikan]').contains('S3').click();
+        cy.get('[data-testid=input-agama]').click();
+        cy.get('[data-testid=input-agama]').contains('Islam').click();
+        cy.get('[data-testid=input-pekerjaan]').click();
+        cy.get('[data-testid=input-pekerjaan]').type('Software Engineer');
+        cy.get('[data-testid=input-nama-kontak-darurat]').type('Dealna');
+        cy.get('[data-testid=input-nomor-kontak-darurat]').type('08121156789');
+        cy.get('[data-testid=input-asuransi]').click();
+        cy.get('[data-testid=input-asuransi]').contains('Tunai').click();
+    });
+});
